75b5584ed40a967038b79568871663ada9da61cd
mohamed_arab1 <momenmoslem991@gmail.com>
2024-07-08 14:07:17 +0300

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+      </Route>
+    )
+  )
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
