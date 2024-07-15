4c50944d557cae6c13e46b016a050861392cd49c
shimaa <shimaa1234maa@gmail.com>
2024-07-15 12:34:18 +0200

    loadingSpaneer

--- src/components/BestSellerBooks.tsx
+++ src/components/BestSellerBooks.tsx
@@ -1,12 +1,11 @@
 import Buttons from "./Buttons";
 import Cards from "./Cards";
 
-
 export default function BestSellerBooks() {
   return (
     <div className="w-full h-full">
-        <Buttons/>
-        <Cards/>
+      <Buttons />
+      <Cards />
     </div>
-  )
+  );
 }
