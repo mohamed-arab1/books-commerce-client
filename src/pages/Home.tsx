ad7ed7f17c9ba6b42cb7bd4369d6ede06825cf1a
WardAbdrabou <wardabdrabou1@gmail.com>
2024-07-15 23:10:59 +0300

    cart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,20 @@
 import BestSellerBooks from "../components/BestSellerBooks";
 import Footer from "../components/Footer";
 import MiddleSection from "../components/MiddleSection";
+import Navbar from "../components/Navbar";
 import SliderSection from "../components/SliderSection";
 
 const Home = () => {
   return (
+    <>
+    <Navbar></Navbar>
     <div className="w-full">
       <SliderSection />
       <BestSellerBooks />
       <MiddleSection />
       <Footer />
     </div>
+    </>
   )
 };
 
